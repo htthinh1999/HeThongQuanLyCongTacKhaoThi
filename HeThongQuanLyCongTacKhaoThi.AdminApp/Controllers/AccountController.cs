4168e90737a4915ffd0a16db4671f12c0d31dc82
Huỳnh Tấn Thịnh <htthinh1999@gmail.com>
2021-01-29 22:19:03 +0700

    update

---
+++ HeThongQuanLyCongTacKhaoThi.AdminApp/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using HeThongQuanLyCongTacKhaoThi.ViewModels.System.Accounts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeThongQuanLyCongTacKhaoThi.AdminApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
